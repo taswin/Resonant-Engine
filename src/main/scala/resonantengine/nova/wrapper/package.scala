7b5e252d08876ccf587befa24d6a44cd83dfcd47
Calclavia <admin@calclavia.com>
2015-01-31 12:03:10 +0800

    Revert "Started migration work to better Vector system"
    This reverts commit f60c4aa2a3f878c2d73f15b78b1aa150de9db2ea.

--- src/main/scala/resonantengine/nova/wrapper/package.scala
+++ src/main/scala/resonantengine/nova/wrapper/package.scala
@@ -1,52 +1,37 @@
 package resonantengine.nova
 
-import nova.core.entity.Entity
-import nova.core.util.transform.{Cuboid, Operator, Vector3d}
-import nova.wrapper.mc1710.backward.entity.EntityBackwardWrapper
-import nova.wrapper.mc1710.forward.util.CuboidForwardWrapper
+import nova.core.util.transform.Operator
 
 /**
  * Wraps NOVA objects and provides them with Scala synthetic sugar coating.
  * @author Calclavia
  */
-package object wrapper {
+package object wrapper
+{
+  implicit class OperatorWrapper[I <: Operator[I, O], O <: I](underlying: Operator[I, O])
+  {
+    def +(other: I): O = underlying.add(other)
 
-	implicit def wrapEntity(underlying: net.minecraft.entity.Entity): Entity = new EntityBackwardWrapper(underlying)
+    def +(other: Double): O = underlying.add(other)
 
-	/**
-	 * Temporary Minecraft Bridge
-	 */
-	implicit class OperatorWrapper[I <: Operator[I, O], O <: I](underlying: Operator[I, O]) {
-		def +(other: I): O = underlying.add(other)
+    def -(other: I): O = underlying.subtract(other)
 
-		def +(other: Double): O = underlying.add(other)
+    def -(other: Double): O = underlying.subtract(other)
 
-		def -(other: I): O = underlying.subtract(other)
+    def *(other: I): O = underlying.multiply(other)
 
-		def -(other: Double): O = underlying.subtract(other)
+    def *(other: Double): O = underlying.multiply(other)
 
-		def *(other: I): O = underlying.multiply(other)
+    def /(other: I): O = underlying.divide(other)
 
-		def *(other: Double): O = underlying.multiply(other)
+    def /(other: Double): O = underlying.divide(other)
 
-		def /(other: I): O = underlying.divide(other)
+    def unary_+ : O = underlying.asInstanceOf[O]
 
-		def /(other: Double): O = underlying.divide(other)
+    def unary_- : O = underlying.inverse()
+  }
 
-		def unary_+ : O = underlying.asInstanceOf[O]
-
-		def unary_- : O = underlying.inverse()
-	}
-
-	implicit class WrapTile(underlying: net.minecraft.tileentity.TileEntity) {
-		def getPosition = new Vector3d(underlying.xCoord, underlying.yCoord, underlying.zCoord)
-	}
-
-	implicit class WrapEntity(underlying: net.minecraft.entity.Entity) {
-		def getPosition = new Vector3d(underlying.posX, underlying.posY, underlying.posZ)
-	}
-
-	implicit class WrapCuboid(underlying: Cuboid) {
-		def toAABB = new CuboidForwardWrapper(underlying)
-	}
+  /**
+   * Temporary Minecraft Bridge
+   */
 }
