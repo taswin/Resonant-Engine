40a80e79c50e40f764df9874be7aec2998fb2819
Calclavia <admin@calclavia.com>
2015-02-19 21:29:04 +0800

    Fixed TileOre class

--- src/main/scala/com/resonant/wrapper/lib/factory/resources/block/TileOre.scala
+++ src/main/scala/com/resonant/wrapper/lib/factory/resources/block/TileOre.scala
@@ -1,10 +1,13 @@
 package com.resonant.wrapper.lib.factory.resources.block
 
-import java.awt.Color
+import java.util.Optional
 
-import com.resonant.wrapper.lib.factory.resources.Resource
+import com.resonant.wrapper.lib.factory.resources.{Resource, ResourceFactory}
 import nova.core.block.Block
-import nova.core.util.transform.Vector3d
+import nova.core.render.Color
+import nova.core.render.model.Model
+import nova.core.render.texture.Texture
+import nova.core.util.Direction
 
 /**
  * A generic ore block that is automatically colored/textured based on a color multiplier.
@@ -17,52 +20,14 @@ class TileOre extends Block with Resource {
 	//"oreForeground"
 	//"oreBackground"
 
-	override def colorMultiplier: Int = 0xFFFFFF
+	override def colorMultiplier(side: Direction): Color = if (renderingForeground) Color.argb(ResourceFactory.getColor(material)) else Color.white
 
-	@SideOnly(Side.CLIENT)
-	override def getIcon: IIcon = if (renderingForeground) Block.icon.get("oreForeground") else super.getIcon
+	override def getTexture(side: Direction): Optional[Texture] = if (renderingForeground) super.getTexture(side) else super.getTexture(side)
 
-	@SideOnly(Side.CLIENT)
-	override def renderInventory(itemStack: ItemStack) {
+	override def renderStatic(model: Model) {
 		renderingForeground = false
-		//Render background
-		GL11.glPushMatrix()
-		RenderUtility.renderNormalBlockAsItem(itemStack.getItem.asInstanceOf[ItemBlock].field_150939_a, itemStack.getItemDamage, RenderUtility.renderBlocks)
-		GL11.glPopMatrix()
-
+		super.renderStatic(model)
 		renderingForeground = true
-		//Render foreground
-		GL11.glPushMatrix()
-		val material = ResourceFactory.getMaterial(itemStack.getItem.asInstanceOf[ItemBlock].field_150939_a)
-		val color = new Color(ResourceFactory.getColor(material))
-		GL11.glColor4f(color.getRed / 255f, color.getGreen / 255f, color.getBlue / 255f, 1)
-		RenderUtility.renderBlocks.setOverrideBlockTexture(Block.icon.get("oreForeground"))
-		RenderUtility.renderNormalBlockAsItem(itemStack.getItem.asInstanceOf[ItemBlock].field_150939_a, itemStack.getItemDamage, RenderUtility.renderBlocks)
-		RenderUtility.renderBlocks.clearOverrideBlockTexture()
-		GL11.glPopMatrix()
-	}
-
-	@SideOnly(Side.CLIENT)
-	override def renderStatic(renderer: RenderBlocks, pos: Vector3d, pass: Int): Boolean = {
-		renderingForeground = false
-		//Render background
-		renderer.renderStandardBlock(block, pos.xi, pos.yi, pos.zi)
-
-		renderingForeground = true
-		//Render foreground
-		val color = new Color(ResourceFactory.getColor(resourceMaterial))
-
-		if (Minecraft.isAmbientOcclusionEnabled && block.getLightValue() == 0) {
-			if (renderer.partialRenderBounds) {
-				renderer.renderStandardBlockWithAmbientOcclusionPartial(block, pos.xi, pos.yi, pos.zi, color.getRed / 255f, color.getGreen / 255f, color.getBlue / 255f)
-			}
-			else {
-				renderer.renderStandardBlockWithAmbientOcclusion(block, pos.xi, pos.yi, pos.zi, color.getRed / 255f, color.getGreen / 255f, color.getBlue / 255f)
-			}
-		}
-		else {
-			renderer.renderStandardBlockWithColorMultiplier(block, pos.xi, pos.yi, pos.zi, color.getRed / 255f, color.getGreen / 255f, color.getBlue / 255f)
-		}
-		return true
+		super.renderStatic(model)
 	}
 }
