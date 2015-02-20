ec9abf1420d7dec746f121ceb1da28e26c47f2ed
Calclavia <admin@calclavia.com>
2015-02-20 16:14:23 +0800

    Some work on FluidUtility

--- src/main/scala/com/resonant/wrapper/lib/utility/FluidUtility.java
+++ src/main/scala/com/resonant/wrapper/lib/utility/FluidUtility.java
@@ -1,103 +1,62 @@
 package com.resonant.wrapper.lib.utility;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockLiquid;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
+import nova.core.block.Block;
+import nova.core.fluid.Fluid;
+import nova.core.fluid.FluidBlock;
+import nova.core.fluid.Tank;
+import nova.core.fluid.TankProvider;
 import nova.core.util.Direction;
 import nova.core.util.collection.Pair;
 import nova.core.util.transform.Vector3d;
-import resonantengine.lib.transform.vector.VectorWorld;
-import resonantengine.lib.utility.inventory.AutoCraftingManager;
-import resonantengine.lib.utility.inventory.InventoryUtility;
+import nova.core.util.transform.Vector3i;
+import nova.core.world.World;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Set;
+import java.util.Optional;
 
 /**
- * Fluid interactions.
- * @author DarkCow, Calclavia
+ * Fluid interaction helper methods.
+ *
+ * @author Calclavia
  */
 public class FluidUtility {
-	public static Set<Pair<Block, Integer>> replacableBlockMeta = new HashSet();
-	public static Set<Block> replacableBlocks = new HashSet();
-	public static Set<Block> nonBlockDropList = new HashSet();
 
-	static {
-
-        /* Adds default fluid replaceable blocks */
-		replacableBlocks.add(Blocks.wheat);
-		replacableBlocks.add(Blocks.deadbush);
-		nonBlockDropList.add(Blocks.deadbush);
-		// TODO have waterlily raise and lower when auto filling or draining a block rather
-		// than remove it
-		replacableBlocks.add(Blocks.waterlily);
-		replacableBlocks.add(Blocks.red_mushroom);
-		replacableBlocks.add(Blocks.brown_mushroom);
-		replacableBlocks.add(Blocks.nether_wart);
-		replacableBlocks.add(Blocks.sapling);
-		replacableBlocks.add(Blocks.melon_stem);
-		nonBlockDropList.add(Blocks.melon_stem);
-		replacableBlocks.add(Blocks.pumpkin_stem);
-		nonBlockDropList.add(Blocks.pumpkin_stem);
-		replacableBlocks.add(Blocks.tallgrass);
-		replacableBlocks.add(Blocks.torch);
+	public static int getFluidAmountFromBlock(World world, Vector3i vector) {
+		Optional<Fluid> fluid = getFluidFromBlock(world, vector);
+		return fluid.isPresent() ? fluid.get().amount() : 0;
 	}
 
-	public static int getFluidAmountFromBlock(World world, Vector3d vector) {
-		FluidStack fluidStack = getFluidStackFromBlock(world, vector);
-		return fluidStack != null ? fluidStack.amount : 0;
-	}
+	public static Optional<Fluid> getFluidFromBlock(World world, Vector3i pos) {
+		Optional<Block> block = world.getBlock(pos);
 
-	public static FluidStack getFluidStackFromBlock(World world, Vector3d vector) {
-		Block block = vector.getBlock(world);
-		int meta = vector.getBlockMetadata(world);
-
-		if (block instanceof IFluidBlock) {
-			IFluidBlock fluidBlock = ((IFluidBlock) block);
-			return new FluidStack(fluidBlock.getFluid(), (int) (FluidContainerRegistry.BUCKET_VOLUME * fluidBlock.getFilledPercentage(world, vector.xi(), vector.yi(), vector.zi())));
-		} else if (block == Blocks.water || block == Blocks.flowing_water) {
-			if (meta == 0) {
-				return new FluidStack(FluidRegistry.WATER, FluidContainerRegistry.BUCKET_VOLUME);
-			}
-		} else if (block == Blocks.lava || block == Blocks.flowing_lava) {
-			if (meta == 0) {
-				return new FluidStack(FluidRegistry.LAVA, FluidContainerRegistry.BUCKET_VOLUME);
-			}
+		if (block.isPresent() && block.get() instanceof FluidBlock) {
+			return ((FluidBlock) block.get()).getFluid();
 		}
 
-		return null;
+		return Optional.empty();
 	}
 
-	public static FluidTankInfo[] getTankInfo(World world, Vector3d posiiton, Direction from) {
-		TileEntity tile = posiiton.getTileEntity(world);
+	public static Optional<Tank> getTank(World world, Vector3d pos, Direction from) {
+		Optional<Block> block = world.getBlock(pos);
 
-		if (tile instanceof IFluidHandler) {
-			if (((IFluidHandler) tile).getTankInfo(from) != null) {
-				return ((IFluidHandler) tile).getTankInfo(from);
-			}
+		if (block.isPresent() && block.get() instanceof TankProvider) {
+			return ((TankProvider) block.get()).getTank(from);
 		}
 
-		return new FluidTankInfo[0];
+		return Optional.empty();
 	}
 
-	public static double getFilledPercentage(FluidTankInfo... info) {
+	public static double getFilledPercentage(Tank... tanks) {
 		int amount = 0;
 		int capacity = 0;
 
-		for (FluidTankInfo tankInfo : info) {
-			if (tankInfo != null && tankInfo.fluid != null) {
-				amount += tankInfo.fluid.amount;
-				capacity += tankInfo.capacity;
-			}
+		for (Tank tank : tanks) {
+			amount += tank.getFluidAmount();
+			capacity += tank.getFluidCapacity();
 		}
 
 		if (capacity > 0) {
@@ -107,15 +66,15 @@ public class FluidUtility {
 		return 0;
 	}
 
-	public static double getAveragePercentageFilledForSides(Class classMask, double defaultFill, World world, Vector3d position, Direction... sides) {
+	public static double getAveragePercentageFilledForSides(Class classMask, double defaultFill, World world, Vector3i pos, Direction... sides) {
 		double fullness = defaultFill;
 		int count = 1;
 
 		for (Direction side : sides) {
-			TileEntity tile = position.clone().add(side).getTileEntity(world);
+			Optional<Block> block = world.getBlock(pos);
 
-			if (tile != null && (classMask == null || classMask.isAssignableFrom(tile.getClass()))) {
-				FluidTankInfo[] info = getTankInfo(world, position.clone().add(side), side);
+			if (block.isPresent() && (classMask == null || classMask.isAssignableFrom(block.get().getClass()))) {
+				FluidTankInfo[] info = getTank(world, pos.clone().add(side), side);
 
 				if (info.length > 0) {
 					fullness += getFilledPercentage(info);
@@ -129,6 +88,7 @@ public class FluidUtility {
 
 	/**
 	 * Gets the block's fluid if it has one
+	 *
 	 * @param world - world we are working in
 	 * @param vector - 3D location in world
 	 * @return @Fluid that the block is
@@ -141,8 +101,8 @@ public class FluidUtility {
 	 * Gets a fluid from blockID
 	 */
 	public static Fluid getFluidFromBlockID(Block block) {
-		if (block instanceof IFluidBlock) {
-			return ((IFluidBlock) block).getFluid();
+		if (block instanceof FluidBlock) {
+			return ((FluidBlock) block).getFluid();
 		} else if (block == Blocks.water || block == Blocks.flowing_water) {
 			return FluidRegistry.WATER;
 		} else if (block == Blocks.lava || block == Blocks.flowing_lava) {
@@ -152,16 +112,16 @@ public class FluidUtility {
 		return null;
 	}
 
-	public static FluidStack getStack(FluidStack stack, int amount) {
+	public static Fluid getStack(Fluid stack, int amount) {
 		if (stack != null) {
-			FluidStack newStack = stack.copy();
+			Fluid newStack = stack.copy();
 			newStack.amount = amount;
 			return newStack;
 		}
 		return null;
 	}
 
-	public static boolean matchExact(FluidStack stack, FluidStack stack2) {
+	public static boolean matchExact(Fluid stack, Fluid stack2) {
 		if (stack == null && stack2 == null) {
 			return true;
 		} else if (stack != null && stack.isFluidEqual(stack2)) {
@@ -172,22 +132,24 @@ public class FluidUtility {
 
 	/**
 	 * Drains a block of fluid
+	 *
 	 * @param doDrain - do the action
-	 * @return FluidStack drained from the block
+	 * @return Fluid drained from the block
 	 * @Note sets the block with a client update only. Doesn't tick the block allowing for better
 	 * placement of fluid that can flow infinitely
 	 */
-	public static FluidStack drainBlock(World world, Vector3d position, boolean doDrain) {
+	public static Fluid drainBlock(World world, Vector3d position, boolean doDrain) {
 		return drainBlock(world, position, doDrain, 3);
 	}
 
 	/**
 	 * Drains a block of fluid
+	 *
 	 * @param doDrain - do the action
 	 * @param update - block update flag to use
-	 * @return FluidStack drained from the block
+	 * @return Fluid drained from the block
 	 */
-	public static FluidStack drainBlock(World world, Vector3d position, boolean doDrain, int update) {
+	public static Fluid drainBlock(World world, Vector3d position, boolean doDrain, int update) {
 		if (world == null || position == null) {
 			return null;
 		}
@@ -196,8 +158,8 @@ public class FluidUtility {
 		int meta = position.getBlockMetadata(world);
 
 		if (block != null) {
-			if (block instanceof IFluidBlock && ((IFluidBlock) block).canDrain(world, position.xi(), position.yi(), position.zi())) {
-				return ((IFluidBlock) block).drain(world, position.xi(), position.yi(), position.zi(), doDrain);
+			if (block instanceof FluidBlock && ((FluidBlock) block).canDrain(world, position.xi(), position.yi(), position.zi())) {
+				return ((FluidBlock) block).drain(world, position.xi(), position.yi(), position.zi(), doDrain);
 			} else if ((block == Blocks.water || block == Blocks.flowing_water) && position.getBlockMetadata(world) == 0) {
 				if (doDrain) {
 					Vector3d vec = position.clone().add(Direction.UP);
@@ -209,12 +171,12 @@ public class FluidUtility {
 						position.setBlock(world, Blocks.air, 0, update);
 					}
 				}
-				return new FluidStack(FluidRegistry.WATER, FluidContainerRegistry.BUCKET_VOLUME);
+				return new Fluid(FluidRegistry.WATER, Fluid.bucketVolume);
 			} else if ((block == Blocks.lava || block == Blocks.flowing_lava) && position.getBlockMetadata(world) == 0) {
 				if (doDrain) {
 					position.setBlock(world, Blocks.air, 0, update);
 				}
-				return new FluidStack(FluidRegistry.LAVA, FluidContainerRegistry.BUCKET_VOLUME);
+				return new Fluid(FluidRegistry.LAVA, Fluid.bucketVolume);
 			}
 		}
 		return null;
@@ -235,7 +197,7 @@ public class FluidUtility {
 			return false;
 		} else if (block.isAir(world, node.xi(), node.yi(), node.zi())) {
 			return true;
-		} else if (!(block instanceof IFluidBlock || block instanceof BlockLiquid) && block.isReplaceable(world, node.xi(), node.yi(), node.zi()) || replacableBlockMeta.contains(new Pair(block, meta)) || replacableBlocks.contains(block)) {
+		} else if (!(block instanceof FluidBlock || block instanceof BlockLiquid) && block.isReplaceable(world, node.xi(), node.yi(), node.zi()) || replacableBlockMeta.contains(new Pair(block, meta)) || replacableBlocks.contains(block)) {
 			return true;
 		}
 		return false;
@@ -255,7 +217,7 @@ public class FluidUtility {
 		// TODO when added change this to call canFill and fill
 		if (drainBlock(world, node, false) != null) {
 			return false;
-		} else if (block instanceof IFluidBlock || block instanceof BlockLiquid) {
+		} else if (block instanceof FluidBlock || block instanceof BlockLiquid) {
 			return meta != 0;
 		}
 		return false;
@@ -265,10 +227,11 @@ public class FluidUtility {
 	 * Helper method to fill a location with a fluid
 	 * <p/>
 	 * Note: This does not update the block to prevent the liquid from flowing
+	 *
 	 * @return
 	 */
-	public static int fillBlock(World world, Vector3d node, FluidStack stack, boolean doFill) {
-		if ((isFillableBlock(world, node) || isFillableFluid(world, node)) && stack != null && stack.amount >= FluidContainerRegistry.BUCKET_VOLUME) {
+	public static int fillBlock(World world, Vector3d node, Fluid stack, boolean doFill) {
+		if ((isFillableBlock(world, node) || isFillableFluid(world, node)) && stack != null && stack.amount >= Fluid.bucketVolume) {
 			if (doFill) {
 				Block block = node.getBlock(world);
 				int meta = node.getBlockMetadata(world);
@@ -286,21 +249,22 @@ public class FluidUtility {
 
 				node.setBlock(world, stack.getFluid().getBlock());
 			}
-			return FluidContainerRegistry.BUCKET_VOLUME;
+			return Fluid.bucketVolume;
 		}
 		return 0;
 	}
 
 	/**
 	 * Fills all instances of IFluidHandler surrounding the origin
-	 * @param stack - FluidStack that will be filled into the tanks
+	 *
+	 * @param stack - Fluid that will be filled into the tanks
 	 * @param doFill - Actually perform the action or simulate action
 	 * @param ignore - Directions to ignore
 	 * @return amount of fluid that was used from the stack
 	 */
-	public static int fillTanksAllSides(World world, Vector3d origin, FluidStack stack, boolean doFill, Direction... ignore) {
+	public static int fillTanksAllSides(World world, Vector3d origin, Fluid stack, boolean doFill, Direction... ignore) {
 		int filled = 0;
-		FluidStack fillStack = stack != null ? stack.copy() : null;
+		Fluid fillStack = stack != null ? stack.copy() : null;
 		for (Direction direction : Direction.VALID_DIRECTIONS) {
 			if (fillStack == null || fillStack.amount <= 0) {
 				return filled;
@@ -321,12 +285,13 @@ public class FluidUtility {
 
 	/**
 	 * Fills an instance of IFluidHandler in the given direction
-	 * @param stack - FluidStack to fill the tank will
+	 *
+	 * @param stack - Fluid to fill the tank will
 	 * @param doFill - Actually perform the action or simulate action
 	 * @param direction - direction to fill in from the origin
 	 * @return amount of fluid that was used from the stack
 	 */
-	public static int fillTankSide(World world, Vector3d origin, FluidStack stack, boolean doFill, Direction direction) {
+	public static int fillTankSide(World world, Vector3d origin, Fluid stack, boolean doFill, Direction direction) {
 		TileEntity entity = origin.clone().add(direction).getTileEntity(world);
 		if (entity instanceof IFluidHandler && ((IFluidHandler) entity).canFill(direction.getOpposite(), stack.getFluid())) {
 			return ((IFluidHandler) entity).fill(direction.getOpposite(), stack, doFill);
@@ -334,9 +299,9 @@ public class FluidUtility {
 		return 0;
 	}
 
-	public static int fillAllTanks(List<IFluidTank> tanks, FluidStack resource, boolean doFill) {
+	public static int fillAllTanks(List<IFluidTank> tanks, Fluid resource, boolean doFill) {
 		int totalFilled = 0;
-		FluidStack fill = resource.copy();
+		Fluid fill = resource.copy();
 
 		for (IFluidTank tank : tanks) {
 			if (fill.amount > 0) {
@@ -351,15 +316,15 @@ public class FluidUtility {
 		return totalFilled;
 	}
 
-	public static FluidStack drainAllTanks(List<IFluidTank> tanks, int amount, boolean doDrain) {
-		FluidStack drain = null;
+	public static Fluid drainAllTanks(List<IFluidTank> tanks, int amount, boolean doDrain) {
+		Fluid drain = null;
 
 		for (IFluidTank tank : tanks) {
 			if (drain != null && drain.amount >= amount) {
 				break;
 			}
 
-			FluidStack drained = tank.drain(amount, false);
+			Fluid drained = tank.drain(amount, false);
 
 			if (drained != null) {
 				if (drain == null) {
@@ -383,7 +348,7 @@ public class FluidUtility {
 		ItemStack current = entityplayer.inventory.getCurrentItem();
 
 		if (current != null && world.getTileEntity(x, y, z) instanceof IFluidHandler) {
-			FluidStack fluid = FluidContainerRegistry.getFluidForFilledItem(current);
+			Fluid fluid = FluidContainerRegistry.getFluidForFilledItem(current);
 			IFluidHandler tank = (IFluidHandler) world.getTileEntity(x, y, z);
 
 			if (fluid != null) {
@@ -395,7 +360,7 @@ public class FluidUtility {
 					return true;
 				}
 			} else {
-				FluidStack available = tank.drain(Direction.fromOrdinal(side), Integer.MAX_VALUE, false);
+				Fluid available = tank.drain(Direction.fromOrdinal(side), Integer.MAX_VALUE, false);
 
 				if (available != null) {
 					ItemStack filled = FluidContainerRegistry.fillFluidContainer(available, current);
@@ -424,7 +389,7 @@ public class FluidUtility {
 		ItemStack current = entityplayer.inventory.getCurrentItem();
 
 		if (current != null) {
-			FluidStack resource = FluidContainerRegistry.getFluidForFilledItem(current);
+			Fluid resource = FluidContainerRegistry.getFluidForFilledItem(current);
 
 			if (resource != null) {
 				if (fillAllTanks(tanks, resource, false) >= resource.amount) {
@@ -436,7 +401,7 @@ public class FluidUtility {
 					return true;
 				}
 			} else {
-				FluidStack available = drainAllTanks(tanks, Integer.MAX_VALUE, false);
+				Fluid available = drainAllTanks(tanks, Integer.MAX_VALUE, false);
 
 				if (available != null) {
 					ItemStack filled = FluidContainerRegistry.fillFluidContainer(available, current);
@@ -469,6 +434,7 @@ public class FluidUtility {
 
 	/**
 	 * Drains an item of fluid and fills the tank with what was drained
+	 *
 	 * @param stack - should it consume the item. Used mainly for creative mode players. This
 	 * does effect the return of the method
 	 * @return Item stack that would be returned if the item was drain of its fluid. Water bucket ->
@@ -476,7 +442,7 @@ public class FluidUtility {
 	 */
 	public static ItemStack drainItem(ItemStack stack, IFluidHandler tank, Direction side) {
 		if (stack != null && tank != null) {
-			FluidStack liquid = FluidContainerRegistry.getFluidForFilledItem(stack);
+			Fluid liquid = FluidContainerRegistry.getFluidForFilledItem(stack);
 			if (liquid != null) {
 				if (tank.fill(side, liquid, true) > 0) {
 					return stack.getItem().getContainerItem(stack);
@@ -488,6 +454,7 @@ public class FluidUtility {
 
 	/**
 	 * Fills an item with fluid from the tank
+	 *
 	 * @param stack - should it consume the item. Used mainly for creative mode players. This
 	 * does effect the return of the method
 	 * @return Item stack that would be returned if the item was filled with fluid. empty bucket ->
@@ -495,8 +462,8 @@ public class FluidUtility {
 	 */
 	public static ItemStack fillItem(ItemStack stack, IFluidHandler tank, Direction side) {
 		if (stack != null && tank != null) {
-			FluidStack liquid = FluidContainerRegistry.getFluidForFilledItem(stack);
-			FluidStack drainStack = tank.drain(side, Integer.MAX_VALUE, false);
+			Fluid liquid = FluidContainerRegistry.getFluidForFilledItem(stack);
+			Fluid drainStack = tank.drain(side, Integer.MAX_VALUE, false);
 			if (liquid == null && drainStack != null) {
 				ItemStack liquidItem = FluidContainerRegistry.fillFluidContainer(drainStack, stack);
 				if (tank.drain(side, FluidContainerRegistry.getFluidForFilledItem(liquidItem), true) != null) {
@@ -508,17 +475,17 @@ public class FluidUtility {
 	}
 
 	/**
-	 * Builds a list of fluidStacks from FluidTankInfo general taken from an instanceof
+	 * Builds a list of Fluids from FluidTankInfo general taken from an instanceof
 	 * IFluidHandler
 	 */
-	public static List<FluidStack> getFluidList(FluidTankInfo... fluidTankInfos) {
-		List<FluidStack> stackList = new ArrayList<FluidStack>();
-		HashMap<FluidStack, Integer> map = new HashMap<FluidStack, Integer>();
+	public static List<Fluid> getFluidList(FluidTankInfo... fluidTankInfos) {
+		List<Fluid> stackList = new ArrayList<Fluid>();
+		HashMap<Fluid, Integer> map = new HashMap<Fluid, Integer>();
 		if (fluidTankInfos != null) {
 			for (int i = 0; i < fluidTankInfos.length; i++) {
 				FluidTankInfo info = fluidTankInfos[i];
 				if (info != null && info.fluid != null) {
-					FluidStack stack = info.fluid;
+					Fluid stack = info.fluid;
 					if (map.containsKey(FluidUtility.getStack(stack, 0))) {
 						map.put(FluidUtility.getStack(stack, 0), map.get(FluidUtility.getStack(stack, 0)) + stack.amount);
 					} else {
@@ -526,9 +493,9 @@ public class FluidUtility {
 					}
 				}
 			}
-			Iterator<Entry<FluidStack, Integer>> it = map.entrySet().iterator();
+			Iterator<Entry<Fluid, Integer>> it = map.entrySet().iterator();
 			while (it.hasNext()) {
-				Entry<FluidStack, Integer> entry = it.next();
+				Entry<Fluid, Integer> entry = it.next();
 				stackList.add(FluidUtility.getStack(entry.getKey(), entry.getValue()));
 			}
 		}
