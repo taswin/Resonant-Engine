ec3b65f5c5113ebb9e412f9d73426e500efadb82
Calclavia <admin@calclavia.com>
2015-02-20 11:39:10 +0800

    Added ASM library

--- src/main/scala/com/resonant/wrapper/lib/asm/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * ASM classes are used to help with class transformations and bytecode injection.
- * The ASM library is based on ChickenBone's ASM library.
- * @author Calclavia
- */
-
-package com.resonant.wrapper.lib.asm;
