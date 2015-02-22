0ef03583bf64f0b9e0b99e9846c6dd5c782e5331
Calclavia <admin@calclavia.com>
2015-02-23 00:23:20 +0800

    Added FluidContainerProvider for Items

---
+++ src/main/scala/com/resonant/wrapper/core/Placeholder.java
@@ -0,0 +1,25 @@
+package com.resonant.wrapper.core;
+
+import nova.core.entity.Entity;
+import nova.core.gui.KeyManager;
+import nova.core.player.Player;
+
+/**
+ * Temporary placeholder class for methods that do not exist yet
+ *
+ * @author Calclavia
+ */
+public class Placeholder {
+
+	public static boolean isHoldingConfigurator(Entity entity) {
+		return false;
+	}
+
+	public static boolean isKeyDown(KeyManager.Key key) {
+		return false;
+	}
+
+	public static void sendChatToPlayer(Player player, String message) {
+
+	}
+}
