46396b501aaaf0917c9a48ed4882a7c9b2f6f769
Calclavia <admin@calclavia.com>
2015-04-08 18:37:34 +0800

    Fixed more general errors

--- src/main/scala/com/resonant/core/prefab/block/InventorySimpleProvider.scala
+++ src/main/scala/com/resonant/core/prefab/block/InventorySimpleProvider.scala
@@ -1,17 +1,18 @@
 package com.resonant.core.prefab.block
 
-import java.util.Optional
+import java.util
 
-import nova.core.inventory.components.InventoryProvider
-import nova.core.inventory.{Inventory, InventorySimple}
+import nova.core.inventory.Inventory
+import nova.core.inventory.components.SidedInventoryProvider
 import nova.core.util.Direction
 
+import scala.collection.convert.wrapAll._
 /**
  * A trait applied to inventory objects.
  */
-trait InventorySimpleProvider extends InventoryProvider {
+trait InventorySimpleProvider extends SidedInventoryProvider {
 
-	protected val inventory: InventorySimple
+	protected val inventory: Inventory
 
-	override def getInventory(side: Direction): Optional[Inventory] = Optional.of(inventory)
+	override def getInventory(side: Direction): util.Set[Inventory] = Set(inventory)
 }
